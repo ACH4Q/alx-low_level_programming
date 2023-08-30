8f9d5dcd9271a26ad4666c8aa276bf5bd10307b8
ACH4Q <achaqmohammedamine1@gmail.com>
2023-08-30 07:19:34 -0700

    11

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * wildcmp - compare two strings
+ * @s1: string to compare
+ * @s2: string to compare
+ * Return: 1 if equal and 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
