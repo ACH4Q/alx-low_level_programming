045cdab97eaf77d9821cdbbec697ce1cd15d883b
ACH4Q <achaqmohammedamine1@gmail.com>
2023-10-05 11:06:38 -0700

    101 fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,24 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <elf.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define BUFFSIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void ELF_check(unsigned char *e_ident, char *str);
+void ELF_magic(unsigned char *e_ident);
+void ELF_class(unsigned char *e_ident);
+void ELF_data(unsigned char *e_ident);
+void ELF_version(unsigned char *e_ident);
+
+void errMessage(int errCode, char *str, int val);
+int _strlen(char *s, int j);
 
 #endif
